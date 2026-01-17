eeda0dab50f3b47fe464c4e4d6b85426b79808d9
ElenaTheAxolotl <elenavasquez0130@gmail.com>
2026-01-17 00:37:01 -0500

    layouts for scences 1-4

--- index.py
+++ index.py
@@ -1,1 +1,115 @@
 # detective game heheheheheh
+
+# Scene 1: Intro
+
+# Description: 
+
+# You stand in the middle of a hotel room, one barely big enough to fit two beds let alone the amount of people currently
+# inspecting it. The most notable part of the room is on the bed nearest to the window. There, under a plastice sheet, lies
+# a dead body. It is heavily suspected that foul play is involved. You, the leading detective on this case, must figure out
+# what happened to the deceased.
+
+# What will you do first? (Enter the letter)
+
+# Choices:
+
+# A: Inspect the body
+# B: Examine the room
+
+# Outcomes: basically just moving to another scene
+
+# A: get info about dead person (Scene 2)
+# B: get info about the room (Scene 3)
+
+# Consequences: none for either
+
+
+# Scene 2: The Body
+
+# Description:
+
+# The deceased is a young man named Lucas Park, a nobody in the grand scheme of things. The file on him you were provided
+# with revealed nothing substantial. The only things of note were that he had no known living relatives and had been
+# arrested with charges of assult and battery, though the charges were dropped before a trial was held.
+# You step to where the body lay on the bed. Lifting the plastic covering you see...a face of peace. There is no hint of
+# pain or fear, almost as though the body is simply sleepiing. It has not been long enough for the body to start decaying. 
+# Peace is all you see...
+# You lower the plastic covering over the body again as you hear your...partner walking over to the body.
+# "You know, I guess being in bed isn't the worst place to die," he says, irritately relaxed. "Though I can't imagine it's
+# a super comfortable bed." You still cannot fathom how he got this job, let alone why you are forced to have him as a
+# work partner.
+
+# "What say you, lead detective? What do you think happened to this guy?"
+
+# Choices:
+
+# A: "Forensics has not confirmed anything yet."
+# B: "It was poison."
+# C: Ignore him
+
+# Outcomes: (will add the actual dialouge later and gotta figure out how to track which scenes have been done)
+
+# A: continue dialogue, option to move to scene 3 or scene 4
+# B: continue dialogue, option to move to scene 3 or scene 4
+# C: continue dialouge, option to move to scene 3 or scene 4
+
+# Consequences:
+
+# A: minus 1 suspicion
+# B: plus 1 suspicion
+# C: nothing
+
+
+# Scene 3: The Room
+
+# Description:
+
+# Looking around the room, there is not much to see. A few forensics workers are looking for evidence, but the room is
+# practically bare, the only furniture being the two beds, a television, and a dresser below the television. There are two
+# suitcases on the floor, one opened with clothes falling out of it while the other is completely shut, as though it was
+# ready to be picked up and leave at a moment's notice.
+# Your attention on the suitcases appearently drew the attention of someones else: your...partner. He walks up to you,
+# asking, 
+
+# "Think the suitcases could lead to something, lead detective?"
+
+# Choices:
+
+# A: "One of them knew they were in danger."
+# B: "Not until we know whose is whose."
+# C: Ignore him
+
+# Outcomes:
+
+# A: continue dialogue, option to move to scene 2 or 4
+# B: continue dialogue, option to move to scene 2 or 4
+# C: continue dialogue, option to move to scene 2 or 4
+
+# Consequences:
+
+# A: plus 1 suspicion
+# B: minus 1 suspicion
+# C: nothing
+
+
+# Scene 4: The Enterance
+
+# Description:
+
+# At the entrance to the room stands one of the members of the hotel's staff and a young man, close to if not the same age
+# as the deceased. They are talking to one of the officers, supposedly about the dead body in the room. You walk to the
+# doorway where they stand, going to get information from them as well, your partner following you like a pathetic hound.
+
+# Who will you speak to first?
+
+# Choices:
+
+# A: The young man
+# B: The staff member
+
+# Outcomes: again, basically just moving scenes
+
+# A: scene 5
+# B: scene 6
+
+# Consequences: none
